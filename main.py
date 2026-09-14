f4c21a6744d7541860135440d4a62c51804aee7a
Bruno <brunommbpro@gmail.com>
2026-09-14 11:23:16 -0300

    Adicionadas configurações de paginação e ordenação dos dados no endpoint get

--- main.py
+++ main.py
@@ -51,9 +51,11 @@ def get_livros(page: int = 1, limit: int = 10, credentials: HTTPBasicCredentials
     start = (page - 1) * limit
     end = start + limit
 
+    livros_ordenados = sorted(meus_livrozinhos.items(), key=lambda x: x[0])
+
     livros_paginados = [
         {"id": id_livro, "nome_livro": livro_data.nome_livro, "autor_livro": livro_data.autor_livro, "ano_livro": livro_data.ano_livro}
-        for id_livro, livro_data in (list(meus_livrozinhos.items()))[start:end]
+        for id_livro, livro_data in livros_ordenados[start:end]
     ]
 
     return {
