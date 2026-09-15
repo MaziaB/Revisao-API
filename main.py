f186c4d5d6f36dffa9650f735db6e09647909d08
Bruno <brunommbpro@gmail.com>
2026-09-15 16:30:30 -0300

    Instalação das bibliotecas e módulos para configuração de um bd local

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from typing import Optional
 import secrets
 import os
 
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./livros.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
 MEU_USUARIO = "admin"
 MINHA_SENHA = "admin"
 
